e5be456cbdbf0fe2ddb89814e42e96fcc5054696
Denisse Lara <dmlaramartin@gmail.com>
2021-10-26 13:55:14 -0400

    Loop image scrap and catch errors

--- webscrapery/images.py
+++ webscrapery/images.py
@@ -2,25 +2,41 @@ from bs4 import BeautifulSoup
 import requests
 from PIL import Image
 from io import BytesIO
+import os
 
 
-def main():
+def start_search():
     search = input("Search for image: ")
     params = {"q": search}
+    dir_name = search.replace(" ", "_").lower()
+
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name)
+
     r = requests.get("https://www.bing.com/images/search", params=params)
 
     soup = BeautifulSoup(r.text, "html.parser")
     # print(soup)
 
     links = soup.findAll("img", {"class": "mimg"})
-    print(links)
 
     count = 1
     for item in links:
-        img_obj = requests.get(item.attrs["src"])
-        img = Image.open(BytesIO(img_obj.content))
-        img.save(f"./scraped_images/{search}{count}.{img.format}", str(img.format).lower())
-        count += 1
+        try:
+            print("Getting", item.attrs["src"])
+            img_obj = requests.get(item.attrs["src"])
+
+            img = Image.open(BytesIO(img_obj.content))
+            img.save(f"./{dir_name}/{search}{count}.{img.format}", str(img.format).lower())
+            count += 1
+        except:
+            print("Could not save image.")
+
+    start_search()
+
+
+def main():
+    start_search()
 
 
 if __name__ == "__main__":
